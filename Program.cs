3b91d7f6353bd06ea0cb5c1694cd81eb37c21113
PopovAzebali <106154882+PopovAzebali@users.noreply.github.com>
2022-05-24 15:04:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,25 +6,15 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            // 1.
-            bool yesOrNot;
-            // 2.
+            bool checkingLogic;
             string name;
-            // 3.
             decimal exactQuantity;
-            // 4.
             int exact;
-            // 5.
             char characterSequence;
-            // 6.
             sbyte negativeInteger;
-            // 7.
             long bigNumber;
-            // 8.
             double productiveButNotAccurate;
-            // 9.
             float productiveButNotVeryAccurate;
-            // 10.
             byte fewValues;
         }
     }
