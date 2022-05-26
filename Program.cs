3e7cd72f829fd05194ae522518e1b95992832e7a
PopovAzebali <106154882+PopovAzebali@users.noreply.github.com>
2022-05-26 14:31:58 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,29 +6,19 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
-            string myName;
-            string age;
-            string hairColor;
-            string eyeColor;
-            string height;
+            string firstName = "Попов";
+            string lastName = "Алексей";
+            string reserve;
 
-            Console.WriteLine("Как Вас зовут :");
-            myName = Console.ReadLine();
-
-            Console.WriteLine("Сколько Вам полных лет :");
-            age = Console.ReadLine();
-
-            Console.WriteLine("Какого цвета у Вас волосы :");
-            hairColor = Console.ReadLine();
-
-            Console.WriteLine("Какого цвета у Вас глаза :");
-            eyeColor = Console.ReadLine();
-
-            Console.WriteLine("Укажите Ваш рост :");
-            height = Console.ReadLine();
-
-            Console.WriteLine("Получается, что Вас зовут " + myName + '.' + " Вам " + age + " лет. " + " Волосы " + hairColor + " ,глаза " + eyeColor + " , рост " + height);
-           
+            Console.WriteLine("До исправления");
+            Console.WriteLine("Имя : " + firstName);
+            Console.WriteLine("Фамилия : " + lastName);
+            reserve = firstName;
+            firstName = lastName;
+            lastName = reserve;
+            Console.WriteLine("После исправления");
+            Console.WriteLine("Имя : " + firstName);
+            Console.WriteLine("Фамилия : " + lastName);
         }
     }
 }
